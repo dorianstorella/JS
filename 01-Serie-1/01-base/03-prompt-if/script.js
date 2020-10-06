c997bcb87a36803d9e1100ca01d44de8aba06ffb
Storella-D <dorianstorella@gmail.com>
2020-10-06 16:30:29 +0200

    changement exercice 03

--- 01-Serie-1/01-base/03-prompt-if/script.js
+++ 01-Serie-1/01-base/03-prompt-if/script.js
@@ -5,8 +5,10 @@
     console.log('%c ', 'padding: 38px; background:url(https://becode.org/app/uploads/2020/03/bc_mailsign_seal.png) no-repeat;');
     console.log('Hello Woods programmer');
     var gateau = prompt("veut tu du gateau");
-    if (gateau==true){
-        document.getElementById("demo").innerHTML = "felicatation"
+    if (gateau=="oui"){
+        confirm("felicatation")
+    } else {
+        confirm ("plus pour moii")
     }
   
   
