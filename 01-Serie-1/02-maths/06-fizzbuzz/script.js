d4304b5dc2c081666e1c89d1841eb103b5cd3111
Storella-D <dorianstorella@gmail.com>
2020-10-08 22:59:42 +0200

    ajout exercice 2.6

---
+++ 01-Serie-1/02-maths/06-fizzbuzz/script.js
@@ -0,0 +1,15 @@
+
+// 02-maths/06-fizzbuzz/script.js - 2.6: fizzbuzz
+
+(() => {
+    for(i=1;i<101 ;i++){                   
+    if (i%3){
+        alert("fizz")}
+            else {
+                alert("fizzbuzz")
+            }
+        }
+ 
+    
+
+})();
