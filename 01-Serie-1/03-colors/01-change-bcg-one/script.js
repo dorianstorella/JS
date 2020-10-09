d0d41d510244e22a9678ad551114675a938f214c
Storella-D <dorianstorella@gmail.com>
2020-10-09 09:57:21 +0200

    ajout exercice 3.1

---
+++ 01-Serie-1/03-colors/01-change-bcg-one/script.js
@@ -0,0 +1,15 @@
+
+// 03-colors/01-change-bcg-one/script.js - 3.1: Bcg one (1)
+
+
+
+document.getElementById("red").addEventListener('click', function (){
+    document.body.style.background ="red";})
+document.getElementById("green").addEventListener('click', function (){
+        document.body.style.background ="green";})  
+document.getElementById("yellow").addEventListener('click', function (){
+         document.body.style.background ="yellow";})
+document.getElementById("blue").addEventListener('click', function (){
+        document.body.style.background ="blue";})
+    
+
