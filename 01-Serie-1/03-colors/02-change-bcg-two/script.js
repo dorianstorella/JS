66e78254040288b3e872db879c1a8b644f29cfb6
Storella-D <dorianstorella@gmail.com>
2020-10-09 11:52:07 +0200

    ajout exercice 3.2

---
+++ 01-Serie-1/03-colors/02-change-bcg-two/script.js
@@ -0,0 +1,9 @@
+
+// 03-colors/02-change-bcg-two/script.js - 3.2: Bcg 2
+
+
+    
+   const body =document.body;
+    const input =document.getElementById("color");
+      document.getElementById("run").addEventListener('click',function(){
+        body.style.background=input.value}) 
