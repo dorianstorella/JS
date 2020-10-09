982188b60dd1ce7b6ed4a700ce0f4a32170e783c
Storella-D <dorianstorella@gmail.com>
2020-10-09 16:00:24 +0200

    NON FINI ajout Ex 4.3

---
+++ 01-Serie-1/04-dates/03-age-by-select/script.js
@@ -0,0 +1,14 @@
+
+// 04-dates/03-age-by-select/script.js - 4.3: calculate the age
+
+
+(() => {
+var jour=document.getElementById("dob-day").value;
+var mois=document.getElementById("dob-month").value;
+var annee=document.getElementById("dob-year").value;
+document.getElementById("run").addEventListener('click',function(){
+alert(Number(parseInt(jour.value)))})
+
+    // your code here
+
+})();
