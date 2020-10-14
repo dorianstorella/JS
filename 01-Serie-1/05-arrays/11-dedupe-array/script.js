682ed5eaa22fcacf287ba0f66ea243b9c15ff247
Storella-D <dorianstorella@gmail.com>
2020-10-14 10:00:55 +0200

    ajout exercice 5.11

---
+++ 01-Serie-1/05-arrays/11-dedupe-array/script.js
@@ -0,0 +1,27 @@
+// 05-arrays/11-dedupe-array/script.js - 5.11: de-dupe the array
+
+
+(() => {
+    
+    const fruits = [
+        "cerise",
+        "durian",
+        "pomme",
+        "poire",
+        "fraise",
+        "tomate",
+        "orange",
+        "mandarine",
+        "fraise",
+        "durian",
+        "pêche",
+        "cerise",
+        "raisin",
+        "cerise",
+    ];
+    console.log(new Set(fruits));
+
+   
+    
+
+})();
