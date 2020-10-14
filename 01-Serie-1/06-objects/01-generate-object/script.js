bc2a2754a6a309b352f6ed226103f34ed6afe38f
Storella-D <dorianstorella@gmail.com>
2020-10-14 14:15:48 +0200

    ajout exercice 6.1

---
+++ 01-Serie-1/06-objects/01-generate-object/script.js
@@ -0,0 +1,13 @@
+// 06-objects/01-generate-object/script.js - 6.1: genererate an object
+
+
+(() => {
+    var MOI = {
+        nom:'Storella',
+        prenom:'Dorian',
+        age:24,
+        ville:'sambreville',
+        pays: 'belgique'
+    }
+    console.log (MOI)
+})();
