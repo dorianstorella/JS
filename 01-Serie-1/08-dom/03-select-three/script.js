d38ca96e35eba5c6a359b48dbc56d9c6ddb431d8
Storella-D <dorianstorella@gmail.com>
2020-10-21 21:33:16 +0200

    ajout exercice 8.3

---
+++ 01-Serie-1/08-dom/03-select-three/script.js
@@ -0,0 +1,11 @@
+// 08-dom/03-select-three/script.js - 8.3: select multiple elements by css selector
+
+
+(() => {
+    document.querySelectorAll('.target').forEach( target => {
+        target.innerHTML="owned"
+    })
+ 
+    
+
+})();
